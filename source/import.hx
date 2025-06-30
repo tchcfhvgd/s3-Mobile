5925fd747333ee5178c491e2cdf9915c736d3894
tchcfhvgd <2220687506@qq.com>
2025-06-30 22:13:18 +0800

    fix again

--- source/import.hx
+++ source/import.hx
@@ -1,6 +1,20 @@
 #if !macro
+// Flixel
+import flixel.sound.FlxSound;
 import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxDestroyUtil;
+import flixel.FlxCamera;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.group.FlxSpriteGroup;
 import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
 
 //Mobile Controls
 import mobile.objects.MobileControls;
